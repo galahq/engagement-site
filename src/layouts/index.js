a6b3039d8d5203bf8faeaa96a82787da7de6e245
Cameron Bothner <cameronbothner@gmail.com>
2018-06-04 09:28:54 -0400

    Add a footer

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import Helmet from 'react-helmet'
 
+import { TextContainer } from '../components/container'
+
 import wave from '../assets/wave.png'
 import './index.css'
 
@@ -16,7 +18,18 @@ const Layout = ({ children, data }) => (
       ]}
     />
     {children()}
-    <Footer />
+    <Footer>
+      <TextContainer>
+        The Citizen Sustainability Certificate is a partnership between{' '}
+        <a href="http://www.teachmsc.org">Michigan Sustainability Cases</a>, a
+        project of the{' '}
+        <a href="http://seas.umich.edu">
+          School of Environment and Sustainability
+        </a>{' '}
+        at the <a href="https://umich.edu">University of Michigan</a>, and the{' '}
+        <a href="https://www.a2gov.org/Pages/default.aspx">City of Ann Arbor</a>.
+      </TextContainer>
+    </Footer>
   </Body>
 )
 export default Layout
@@ -41,5 +54,9 @@ const Body = styled.div`
 `
 
 const Footer = styled.footer`
-  padding: 2rem 0;
+  background-color: #e1f3f5;
+  border-top: 1px solid #bfe0e4;
+  font-size: 0.9em;
+  margin-top: 4rem;
+  padding: 3rem 0 4rem;
 `
