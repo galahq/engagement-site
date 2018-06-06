2a8a8052ee370df29a0f710b807eb7b4e2c9808c
Cameron Bothner <cameronbothner@gmail.com>
2018-06-06 08:57:34 -0400

    Update language with Meghan’s changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const IndexPage = () => (
   <div>
     <Title>
       <span>
-        Michigan Sustainability Cases<br />& The City of Ann Arbor
+        University of Michigan &<br />The City of Ann Arbor
       </span>
       Citizen<br />Sustainability<br />Certificate
     </Title>
@@ -22,43 +22,33 @@ const IndexPage = () => (
       <p>
         Never have we needed more hands to build bridges between theory and
         practice. Sustainability is fundamentally about what we can do now to
-        take care of generations to come, and we need more and deeper
-        connections among those of us facing challenges across the globe. While
-        concerned equally with human and nonhuman life, it is mired in human
-        relationships. Our field of action is strewn with terms like “political
-        impasse,” “cultural barrier,” and “knowledge gap.”
+        take care of future generations, an endeavor that requires more and
+        deeper connections among people across the globe. While concerned
+        equally with human and nonhuman life, sustainability is mired in human
+        relationships.{' '}
       </p>
-
       <p>
-        Such problems cannot be solved by classroom teaching alone. Curricular
-        tools must connect to social fabrics to strengthen and stretch them.
-        They must make learning relevant to lived challenges. They must enable
-        us to more creatively confront the messes we have made…even the ones we
-        don’t yet know how to clean up.
+        Such problems cannot be solved by classroom teaching alone. Educational
+        tools must strengthen social fabrics, make learning relevant to lived
+        challenges, and enable us to more creatively confront the messes we have
+        made…even the ones we don’t yet know how to clean up.
       </p>
-
       <p>
         That is why we are launching a pilot “Citizen Sustainability
-        Certificate” program, featuring some of our best cases studies. They
-        have been used in University of Michigan classrooms or field and lab
+        Certificate” program, featuring some of our best case studies. They have
+        been used in University of Michigan classrooms or field and lab
         settings. We have gathered information from teachers, students and
         practitioners about what worked well and what didn’t. We have revised
         and relaunched them, and now they are ready for you—for all of us.
       </p>
-
-      <h2>A focus on water quality</h2>
-
+      <h2 id="a-focus-on-water-quality">A focus on water quality</h2>
       <p>
         The three case studies we have selected for this year are all focused on
         aspects of water quality: drinking water, stormwater, and recreational
-        waters. They will help you understand scientific content like fluid
-        dynamics, water ecosystems, chemical toxicity, and how risks to human
-        health are evaluated. They will confer competencies including a working
-        introduction to ecological and human risk assessment, data entry for
-        basic water sampling, and comparative performance analysis of water
-        infrastructure. And they will inspire citizenship skills by encouraging
-        cross site comparative thinking, intercultural collaboration, critical
-        analysis, and communication.
+        waters. They will help you understand complex scientific concepts and
+        impart citizenship skills by encouraging cross site comparative
+        thinking, intercultural collaboration, critical analysis, and
+        communication.
       </p>
     </TextContainer>
 
@@ -85,38 +75,26 @@ const IndexPage = () => (
 
     <TextContainer>
       <p>
-        For those who complete these three water cases, a certificate will be
-        conferred by the City of Ann Arbor and the University of Michigan School
-        for Environment and Sustainability at next year’s Galaxy event. At that
-        time, a new slate of three cases will launch for certification. Climate
-        change? Wildlife management? Renewable energy? Join us in establishing
-        the priority list, producing the modules themselves, and then testing,
-        assessing and iteratively improving them. We can make our communities
-        work better, and work as a model for other communities seeking to bridge
-        campus/civic divides for positive change.
+        For those who complete these three water cases, the City of Ann Arbor
+        and the University of Michigan School for Environment and Sustainability
+        will confer a certificate at next year’s Galaxy event. At that time, a
+        new slate of three cases will launch for certification. Climate change?
+        Wildlife management? Renewable energy? Join us in establishing the
+        priority list, producing the modules themselves, and then improving
+        them.{' '}
       </p>
-
-      <h2>Join us</h2>
-
+      <h2 id="join-us">Join us</h2>
       <p>
         We built Gala, the website that hosts the cases you’ll read, to collect
-        teaching cases on sustainability, and to enable collaboration between
-        across case teams, classrooms, and real world contexts where case are
-        used. Anyone can create, publish, and continually update a case about
+        teaching cases on sustainability, and to enable collaboration across the
+        world. Anyone can create, publish, and continually update a case about
         any topic they want. We named it for an apple variety commonly grown in
         Michigan. But the name also evokes a party where people come together to
-        combine their resources for further work in the world. Because making
+        combine their resources for innovative work in the world. Because making
         cleaner, safer, more efficient and resilient systems for water, food,
         waste, energy, transport, recreation, and more is worth celebrating—and
-        worth supporting.
-      </p>
-
-      <p>
-        One of the most important ways you can support us is to join us in
-        learning and teaching on these challenging topics. Be the hands that
-        build the bridges within our community, between communities or even
-        fields of knowledge, and between this moment and the future we want to
-        make from it.
+        worth supporting. And so we invite you to join us in learning and
+        teaching, and to be the hands that build bridges.
       </p>
     </TextContainer>
   </div>
