136f49ae16d681109fea267f8f3ce594e47ce270
waisaed <waisaed@umich.edu>
2020-01-28 18:53:12 -0500

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: 'Briefing: from Gala',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-85341915-2',
+      },
+    },
+  ],
 }
