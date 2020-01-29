ea847db190798fd3d1b33529f0e056ce609fa821
waisaed <waisaed@umich.edu>
2020-01-29 12:38:10 -0500

    Add resource card

---
+++ src/components/cards/resource.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import BaseCard from '../card'
+
+export default ({ title, description, href }) => (
+  <Card href={href}>
+    <Title>{title}</Title>
+    <Description>{description}</Description>
+  </Card>
+)
+
+const Card = BaseCard.withComponent('a').extend`
+  color: #192223;
+  text-decoration: none;
+  display: inline-block;
+  padding: 1em;
+  hyphens: manual;
+  width: 80%;
+  &:hover {
+    box-shadow: none;
+  }
+`
+
+const Title = styled.a`
+  text-decoration: underline;
+  font-size: 1em;
+  font-weight: 600;
+`
+
+const Description = styled.p`
+  font-size: 0.95em;
+  font-style: italic;
+  line-height: 1.1;
+  margin-top: 0.5em;
+`
