fa809eda800136da39cd3e191e05b626c62d499e
waisaed <waisaed@umich.edu>
2020-01-28 19:16:40 -0500

    remove google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,14 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'Briefing: from Gala',
   },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-85341915-2',
-      },
-    },
-  ],
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
 }
